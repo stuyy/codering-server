43a3bcbe43ed889038948766ed4a674748d7e0a7
Anson Foong <ansfoong@gmail.com>
2020-06-22 05:57:10 -0400

    Added GithubService class to call Github API

---
+++ src/services/external/github.service.ts
@@ -0,0 +1,18 @@
+
+/**
+ * GithubService is a class that wraps external API calls to Github API
+ */
+export default class GithubService {
+
+  static async postWebhook() {
+
+  }
+
+  static async getWebhooks() {
+
+  }
+
+  static async getWebhook() {
+    
+  }
+}
