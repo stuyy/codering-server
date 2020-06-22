27414d59c3a6a9a79ebdd958fd9683297b69f4d7
Anson Foong <ansfoong@gmail.com>
2020-06-22 05:57:00 -0400

    Updated UserSchema

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -2,27 +2,39 @@ import mongoose from 'mongoose';
 
 export const UserSchema = new mongoose.Schema({
   githubId: {
-    type: mongoose.Schema.Types.String,
+    type: mongoose.SchemaTypes.String,
     required: true,
     unique: true,
   },
   username: {
-    type: mongoose.Schema.Types.String,
+    type: mongoose.SchemaTypes.String,
     required: true,
   },
   displayName: {
-    type: mongoose.Schema.Types.String,
+    type: mongoose.SchemaTypes.String,
     required: false,
   },
   avatar: {
-    type: mongoose.Schema.Types.String,
+    type: mongoose.SchemaTypes.String,
     required: true,
   },
   profileUrl: {
-    type: mongoose.Schema.Types.String,
+    type: mongoose.SchemaTypes.String,
     required: true,
   },
   roles: [],
+  discordAuthed: {
+    type: mongoose.SchemaTypes.Boolean,
+    default: false
+  },
+  discordId: {
+    type: mongoose.SchemaTypes.String,
+    default: null,
+  },
+  discordTag: {
+    type: mongoose.SchemaTypes.String,
+    default: null,
+  }
 });
 
 const User = mongoose.model('user', UserSchema);
