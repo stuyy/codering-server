f502c50121640d651d8da4018b8544563fe5fc95
Anson Foong <ansfoong@gmail.com>
2020-07-04 15:01:20 -0400

    Added organization route

--- src/routes/github/index.ts
+++ src/routes/github/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import WebhookRoute from './webhooks';
 import PullRequestRoute from './pull-request';
+import OrganizationRoute from './organizations';
+
 const router = Router();
 
 router.use('/pull-request', PullRequestRoute);
 router.use('/webhook', WebhookRoute);
+router.use('/organizations', OrganizationRoute);
 
 export default router;
